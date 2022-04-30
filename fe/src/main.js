98b56870b1c2c3e71cf25c422870cf463c085455
Yvan Sabay <sabayyvan2018@gmail.com>
2022-04-30 18:18:04 +0800

    added home

--- fe/src/main.js
+++ fe/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
-
+import './assets/css/styles.css'
 Vue.config.productionTip = false
 
 import { registerModules } from "./register-modules";
---
+++ fe/src/views/components/Navbar.vue
@@ -0,0 +1,87 @@
+<template>
+  <nav>
+    <div class="logo">
+      <p>IMarket</p>
+    </div>
+    <ul>
+      <li>
+        <a href="">Market</a>
+      </li>
+      <li>
+        <a href="">Seller</a>
+      </li>
+      <li>
+        <a href="">Contact</a>
+      </li>
+    </ul>
+    <div class="login">
+      <v-btn class="white--text rounded-pill" color="orange darken-2" depressed large> Login </v-btn>
+    </div>
+  </nav>
+</template>
+<script>
+  export default {
+    data: () => ({
+      name: ''
+    }),
+    mounted() {},
+    methods: {},
+    computed: {},
+    watch: {},
+  };
+</script>
+
+<style lang="scss">
+  .logo p {
+    font-size: 1.4rem;
+    font-weight: 600;
+  }
+
+  nav {
+    position: fixed;
+    width: 100%;
+    padding: 2rem 2.8rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    z-index: 5;
+
+    & ul {
+      display: flex;
+      list-style: none;
+
+      & li {
+        padding: 0 1rem;
+
+        & a {
+          text-decoration: none;
+          color: rgb(29, 29, 29) !important;
+          position: relative;
+          // transition: all 250ms ease-in-out;
+          text-transform: uppercase;
+          font-weight: 600;
+
+          &:hover {
+            color: #ff9800 !important;
+          }
+
+          &:hover:before {
+            width: 100%;
+            transform-origin: right;
+          }
+
+          &:before {
+            content: '';
+            position: absolute;
+            width: 0;
+            bottom: -5px;
+            left: 0;
+            height: 2.5px;
+            background-color: #ff9800 !important;
+            transition: all 250ms ease-in-out;
+          }
+        }
+      }
+    }
+  }
+</style>
